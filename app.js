9532443b403a1261f3fe3224c585ddd1c2271652
Erin McMackin <mcmackinerin@gmail.com>
2018-05-12 15:01:12 -0400

    outline of portfolio created

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
-// $(()=>{
-//   let showAbout = false;
-//   let showPortfolio = true;
-//   let showResume = false;
-//
-//   // go to nav
-//   navAbout = ()=>{
-//     console.log("hi der lil babe");
-//
-//   }
-//
-// });
-
-
 const app = angular.module('MyPortfolio', []);
 
-this.showAbout = false;
 this.showPortfolio = true;
+this.showAbout = false;
 this.showResume = false;
 
+console.log(this.showPortfolio);
+
 app.controller('MyPortfolio', ['$http', function($http){
+
   this.navAbout = ()=>{
     console.log('about clicked');
     this.showAbout = true;
     this.showPortfolio = false;
     this.showResume = false;
   }
+
   this.navPortfolio = ()=>{
     console.log('portfolio clicked');
     this.showAbout = false;
     this.showPortfolio = true;
     this.showResume = false;
   }
+
   this.navResume = ()=>{
     console.log('resume clicked');
     this.showAbout = false;
